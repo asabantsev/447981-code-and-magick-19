4c9c39b7c624fca554b8519c44441c0660f0af42
asabantsev <asabantsev@gmail.com>
2020-03-09 11:46:16 +0300

    Добавлено получение персонажей с сервера и отправка данных волшебника

--- js/setup.js
+++ js/setup.js
@@ -1,8 +1,6 @@
 'use strict';
 
 (function () {
-  var WIZARD_NAMES = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
-  var WIZARD_SURNAMES = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
   var WIZARD_COATCOLORS = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
   var WIZARD_EYESCOLORS = ['black', 'red', 'blue', 'yellow', 'green'];
   var WIZARD_FIREBALLCOLORS = ['#ee4830', '#30a8ee', '#5ce6c0', '#e848d5', '#e6e848'];
@@ -11,34 +9,15 @@
   var similarListElement = document.querySelector('.setup-similar-list');
   var similarWizardTemplate = document.querySelector('#similar-wizard-template').content.querySelector('.setup-similar-item');
 
-  var createWizardsArray = function (length) {
-    for (var i = 0, array = []; i <= length; i++) {
-      array.push({
-        name: WIZARD_NAMES[Math.floor(WIZARD_NAMES.length * Math.random())],
-        surname: WIZARD_SURNAMES[Math.floor(WIZARD_SURNAMES.length * Math.random())],
-        coatColor: WIZARD_COATCOLORS[Math.floor(WIZARD_COATCOLORS.length * Math.random())],
-        eyeColor: WIZARD_EYESCOLORS[Math.floor(WIZARD_COATCOLORS.length * Math.random())]
-      });
-    }
-    return array;
-  };
-
   var renderWizard = function (wizard) {
     var wizardElement = similarWizardTemplate.cloneNode(true);
 
-    wizardElement.querySelector('.setup-similar-label').textContent = wizard.name + ' ' + wizard.surname;
-    wizardElement.querySelector('.wizard-coat').style.fill = wizard.coatColor;
-    wizardElement.querySelector('.wizard-eyes').style.fill = wizard.eyeColor;
+    wizardElement.querySelector('.setup-similar-label').textContent = wizard.name;
+    wizardElement.querySelector('.wizard-coat').style.fill = wizard.colorCoat;
+    wizardElement.querySelector('.wizard-eyes').style.fill = wizard.colorEyes;
 
     return wizardElement;
   };
-
-  var fragment = document.createDocumentFragment();
-  var wizards = createWizardsArray(WIZARD_NUMBERS - 1);
-  for (var i = 0; i < wizards.length; i++) {
-    fragment.appendChild(renderWizard(wizards[i]));
-  }
-  similarListElement.appendChild(fragment);
 
   var bigWizard = document.querySelector('.setup-player');
   var wizardCoat = bigWizard.querySelector('.wizard-coat');
@@ -51,4 +30,38 @@
   window.colorize(wizardCoat, wizardInputCoat, WIZARD_COATCOLORS);
   window.colorize(wizardEyes, wizardInputEyes, WIZARD_EYESCOLORS);
   window.colorize(wizardFireballWrap, wizardInputFireball, WIZARD_FIREBALLCOLORS);
+
+  var successHandler = function (wizards) {
+    var fragment = document.createDocumentFragment();
+
+    for (var i = 0; i < WIZARD_NUMBERS; i++) {
+      fragment.appendChild(renderWizard(wizards[i]));
+    }
+    similarListElement.appendChild(fragment);
+
+    window.dialog.userDialog.querySelector('.setup-similar').classList.remove('hidden');
+  };
+
+  var errorHandler = function (errorMessage) {
+    var node = document.createElement('div');
+    node.style = 'z-index: 100; margin: 0 auto; text-align: center; background-color: red;';
+    node.style.position = 'absolute';
+    node.style.left = 0;
+    node.style.right = 0;
+    node.style.fontSize = '30px';
+
+    node.textContent = errorMessage;
+    document.body.insertAdjacentElement('afterbegin', node);
+  };
+
+  var form = window.dialog.userDialog.querySelector('.setup-wizard-form');
+  form.addEventListener('submit', function (evt) {
+    window.backend.save(new FormData(form), function () {
+      window.dialog.closeDialog();
+    });
+
+    evt.preventDefault();
+  });
+
+  window.backend.load(successHandler, errorHandler);
 })();
--- js/util.js
+++ js/util.js
@@ -4,16 +4,20 @@
   var ESC_KEY = 'Escape';
   var ENTER_KEY = 'Enter';
 
-  window.util = {
-    isEscEvent: function (evt, action) {
-      if (evt.key === ESC_KEY) {
-        action();
-      }
-    },
-    isEnterEvent: function (evt, action) {
-      if (evt.key === ENTER_KEY) {
-        action();
-      }
+  var isEscEvent = function (evt, action) {
+    if (evt.key === ESC_KEY) {
+      action();
     }
+  };
+
+  var isEnterEvent = function (evt, action) {
+    if (evt.key === ENTER_KEY) {
+      action();
+    }
+  };
+
+  window.util = {
+    isEscEvent: isEscEvent,
+    isEnterEvent: isEnterEvent
   };
 })();
